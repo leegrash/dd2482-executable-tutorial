44855fad947b195b9395b076cab426109e6d0f11
Alexander Liu <115296161+02alexliu@users.noreply.github.com>
2024-10-14 23:04:15 +0200

    changed code for Killercoda git pull

--- Simple-Calculator-master/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['./src/__tests__/setup.js'],
-  testMatch: ['**/__tests__/**/*.test.js'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  }
-};
