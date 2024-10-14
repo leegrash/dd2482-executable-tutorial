a46fa52cc85175535417b19de3bedb52ce512885
Alexander Liu <115296161+02alexliu@users.noreply.github.com>
2024-10-15 01:12:32 +0200

    revert

---
+++ Simple-Calculator-master/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./src/__tests__/setup.js'],
+  testMatch: ['**/__tests__/**/*.test.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  }
+};
